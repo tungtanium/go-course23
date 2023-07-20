7e2d2ca1e79aba540460e8cf8fe7e9864432bf15
Tung <hi@tungpham.com>
2023-07-20 13:51:44 +1000

    Fix input order to first, last, middle, country code

--- ex01/main.go
+++ ex01/main.go
@@ -20,16 +20,20 @@ func validate_country_code(code string) bool {
 	return true
 }
 
-func reverse_name_order(name_to_reverse []string) string {
+func order_name(name_to_reverse []string, reverse string) string {
 	name_length := len(name_to_reverse)
 	first_name := name_to_reverse[0]
-	last_name := name_to_reverse[name_length-1]
+	last_name := name_to_reverse[1]
 
 	if name_length == 2 {
 		return fmt.Sprintf("%s %s", last_name, first_name)
 	}
-	middle_name := strings.Join(name_to_reverse[1:name_length-1], " ")
-	return fmt.Sprintf("%s %s %s", last_name, middle_name, first_name)
+	middle_name := strings.Join(name_to_reverse[2:], " ")
+	if reverse == "reverse" {
+		return fmt.Sprintf("%s %s %s", last_name, middle_name, first_name)
+	} else {
+		return fmt.Sprintf("%s %s %s", first_name, middle_name, last_name)
+	}
 }
 
 func format_name_by_country(inputName []string, countryCode string) string {
@@ -49,10 +53,11 @@ func format_name_by_country(inputName []string, countryCode string) string {
 		// https://gist.github.com/ssskip/5a94bfcd2835bf1dea52#file-iso3166-1-alpha2-json
 		// https://obaninternational.com/blog/how-do-naming-conventions-vary-around-the-world
 		case "CN", "JP", "KR", "KP", "VN", "TW", "HU", "BA", "HR", "RS":
-			reversed := reverse_name_order(inputName)
-			return reversed
+			out := order_name(inputName, "reverse")
+			return out
 		default:
-			return strings.Join(inputName, " ")
+			out := order_name(inputName, "noreverse")
+			return out
 		}
 	}
 }
@@ -65,6 +70,7 @@ func main() {
 	if len(inputName) < 1 {
 		fmt.Println("No name to order")
 	}
+	fmt.Println("[INFO] Input (First, Last, Middle, Country Code): ", inputName, countryCode)
 	fmt.Println("Result: ", format_name_by_country(inputName, countryCode))
 
 }
