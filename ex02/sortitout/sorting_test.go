debdccb40fd6e31e2678b6a1fe3b4cc576fea273
Tung <hi@tungpham.com>
2023-08-04 12:31:17 +1000

    Ex04 - 2/4: Update more unittesting for Ex02

--- ex02/sortitout/sorting_test.go
+++ ex02/sortitout/sorting_test.go
@@ -2,8 +2,21 @@ package sortitout
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
+
+// Stack Oh.flow
+func SliceToArray(in interface{}) interface{} {
+	s := reflect.ValueOf(in)
+	if s.Kind() != reflect.Slice {
+		panic("not a slice")
+	}
+	t := reflect.ArrayOf(s.Len(), s.Type().Elem())
+	a := reflect.New(t).Elem()
+	reflect.Copy(a, s)
+	return a.Interface()
+}
 
 func TestValidateInt(t *testing.T) {
 	testCases := []struct {
@@ -69,23 +82,70 @@ func TestValidateString(t *testing.T) {
 	}
 }
 
-// func TestSortString(t *testing.T) {
-// 	testCases := []struct {
-// 		input    []string
-// 		expected []string
-// 	}{
-// 		{[]string{"Mot", "Hai", "Ba", "Bon"}, []string{"Ba", "Bon", "Hai", "Mot"}},
-// 		{[]string{"Theta", "Omega", "Beta", "Gamma"}, []string{"Beta", "Gamma", "Omega", "Theta"}},
-// 		{[]string{"Apple", "Absolute", "Adventure", "Anger"}, []string{"Absolute", "Adventure", "Anger", "Apple"}},
-// 	}
+func TestSortInt(t *testing.T) {
+	testCases := []struct {
+		input    []string
+		expected []int
+	}{
+		{[]string{"4", "2", "8", "9"}, []int{2, 4, 8, 9}},
+		{[]string{"1099", "1111", "1001", "1201"}, []int{1001, 1099, 1111, 1201}},
+		{[]string{"13", "34", "47", "76"}, []int{13, 34, 47, 76}},
+	}
 
-// 	for _, tc := range testCases {
-// 		t.Run(fmt.Sprintf("Input: %s", tc.input), func(t *testing.T) {
-// 			ret := SortString(tc.input)
-// 			// Need to convert ret & tc.expected to array to compare
-// 			if bytes.Compare(ret, tc.expected) != 0 {
-// 				t.Errorf("Input: %s, expected %s, but got %s", tc.input, tc.expected, ret)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Input: %s", tc.input), func(t *testing.T) {
+			ret := SortInt(tc.input)
+			ret_arr := SliceToArray(ret)
+			expected_arr := SliceToArray(tc.expected)
+
+			if ret_arr != expected_arr {
+				t.Errorf("Input: %s, expected %v, but got %v", tc.input, tc.expected, ret)
+			}
+		})
+	}
+}
+
+func TestSortFloat(t *testing.T) {
+	testCases := []struct {
+		input    []string
+		expected []float64
+	}{
+		{[]string{"0.01", "3.14", "7.8", "9.0"}, []float64{0.01, 3.14, 7.8, 9.0}},
+		{[]string{"4.2", "2.0", "8", "0.0003"}, []float64{0.0003, 2.0, 4.2, 8}},
+		{[]string{"20.4", "40.78", "33.3", "0"}, []float64{0, 20.4, 33.3, 40.78}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Input: %s", tc.input), func(t *testing.T) {
+			ret := SortFloat(tc.input)
+			ret_arr := SliceToArray(ret)
+			expected_arr := SliceToArray(tc.expected)
+
+			if ret_arr != expected_arr {
+				t.Errorf("Input: %s, expected %v, but got %v", tc.input, tc.expected, ret)
+			}
+		})
+	}
+}
+
+func TestSortString(t *testing.T) {
+	testCases := []struct {
+		input    []string
+		expected []string
+	}{
+		{[]string{"Mot", "Hai", "Ba", "Bon"}, []string{"Ba", "Bon", "Hai", "Mot"}},
+		{[]string{"Theta", "Omega", "Beta", "Gamma"}, []string{"Beta", "Gamma", "Omega", "Theta"}},
+		{[]string{"Apple", "Absolute", "Adventure", "Anger"}, []string{"Absolute", "Adventure", "Anger", "Apple"}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Input: %s", tc.input), func(t *testing.T) {
+			ret := SortString(tc.input)
+			retArr := SliceToArray(ret)
+			expectedArr := SliceToArray(tc.expected)
+			if retArr != expectedArr {
+				t.Errorf("Input: %s, expected %s, but got %s", tc.input, tc.expected, ret)
+			}
+		})
+	}
+}
